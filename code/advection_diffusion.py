0331c013af4e61b94ce4b5076b33df01fedf8141
jorgensd <dokken92@gmail.com>
2025-02-07 09:03:35 +0100

    Make code run with dolfinx 0.6,0.7,0.8,0.9 and nightly

--- code/advection_diffusion.py
+++ code/advection_diffusion.py
@@ -1,25 +1,46 @@
 """
-    FEniCSx implementation of the proximal Galerkin method for advection-diffusion introduced in [1].
+FEniCSx implementation of the proximal Galerkin method for advection-diffusion introduced in [1].
 
-    [1] Keith, B. and Surowiec, T. (2023) Proximal Galerkin: A structure-
-       preserving finite element method for pointwise bound constraints.
-       arXiv:2307.12444 [math.NA]
+[1] Keith, B. and Surowiec, T. (2023) Proximal Galerkin: A structure-
+   preserving finite element method for pointwise bound constraints.
+   arXiv:2307.12444 [math.NA]
 """
 
+from __future__ import annotations
 import argparse
 from math import pi
 from pathlib import Path
-
+import typing
 import numpy as np
 import pandas as pd
 import ufl
-from dolfinx import fem, io, log, mesh
+from dolfinx import fem, io, log, mesh, __version__ as dolfinx_version
 from dolfinx.fem.petsc import NonlinearProblem
 from dolfinx.mesh import CellType, GhostMode
 from dolfinx.nls.petsc import NewtonSolver
 from mpi4py import MPI
 from petsc4py.PETSc import ScalarType
-from ufl import dot, dx, exp, grad, inner, cos, tanh, max_value, min_value, TrialFunction, TestFunction, ln
+from packaging.version import Version
+from ufl import (
+    dot,
+    dx,
+    exp,
+    grad,
+    inner,
+    cos,
+    tanh,
+    max_value,
+    min_value,
+    TrialFunction,
+    TestFunction,
+    ln,
+)
+
+if typing.TYPE_CHECKING:
+    if Version(dolfinx_version) >= Version("0.7.0"):
+        from dolfinx.fem import Form as _FormType
+    else:
+        from dolfinx.fem import FormMetaClass as _FormType
 
 
 def rank_print(string: str, comm: MPI.Comm, rank: int = 0):
@@ -33,7 +54,7 @@ def rank_print(string: str, comm: MPI.Comm, rank: int = 0):
         print(string)
 
 
-def allreduce_scalar(form: fem.FormMetaClass, op: MPI.Op = MPI.SUM) -> np.floating:
+def allreduce_scalar(form: _FormType, op: MPI.Op = MPI.SUM) -> np.floating:
     """Assemble a scalar form over all processes and perform a global reduction
 
     :param form: Scalar form
@@ -41,6 +62,7 @@ def allreduce_scalar(form: fem.FormMetaClass, op: MPI.Op = MPI.SUM) -> np.floati
     """
     comm = form.mesh.comm
     return comm.allreduce(fem.assemble_scalar(form), op=op)
+
 
 def create_mesh(m: int):
     num_cells_per_direction = 2**m
@@ -53,17 +75,20 @@ def create_mesh(m: int):
     )
     return msh
 
+
 def exact_solution(x, epsilon):
     lambda1 = pi**2 * epsilon
-    r1 =  (1.0 + np.sqrt(1.0 + 4.0 * epsilon * lambda1)) / (2*epsilon)
-    r2 =  (1.0 - np.sqrt(1.0 + 4.0 * epsilon * lambda1)) / (2*epsilon)
-    denom = 2.0 * ( np.exp(-r2) - np.exp(-r1) )
-    u_exact = ( exp( r2 * (x[0] - 1.0) ) - exp( r1 * (x[0] - 1.0) ) ) \
-              * cos(pi * x[1] ) / denom + 0.5
+    r1 = (1.0 + np.sqrt(1.0 + 4.0 * epsilon * lambda1)) / (2 * epsilon)
+    r2 = (1.0 - np.sqrt(1.0 + 4.0 * epsilon * lambda1)) / (2 * epsilon)
+    denom = 2.0 * (np.exp(-r2) - np.exp(-r1))
+    u_exact = (exp(r2 * (x[0] - 1.0)) - exp(r1 * (x[0] - 1.0))) * cos(
+        pi * x[1]
+    ) / denom + 0.5
     return u_exact
 
+
 def BG_solve(
-    m: int,
+    msh: mesh.Mesh,
     polynomial_order: int,
     epsilon: float,
 ):
@@ -71,43 +96,51 @@ def BG_solve(
     Solve the Eriksson–Johnson model problem with Bubnov–Galerkin
 
     """
-    # Create mesh
-    msh = create_mesh(m)
 
     # Define FE subspaces
-    V = fem.FunctionSpace(msh, ("Lagrange", polynomial_order))
+    if Version(dolfinx_version) >= Version("0.8.0"):
+        V = fem.functionspace(msh, ("Lagrange", polynomial_order))
+    else:
+        V = fem.FunctionSpace(msh, ("Lagrange", polynomial_order))
 
     # Define functions and parameters
     x = ufl.SpatialCoordinate(msh)
     f = fem.Constant(msh, 0.0)
     u_exact = exact_solution(x, epsilon)
-    beta = fem.Constant(msh, ScalarType((1,0)))
+    beta = fem.Constant(msh, ScalarType((1, 0)))
 
     # Define BCs
     msh.topology.create_connectivity(msh.topology.dim - 1, msh.topology.dim)
     facets = mesh.exterior_facet_indices(msh.topology)
     dofs = fem.locate_dofs_topological(V, entity_dim=1, entities=facets)
 
+    if Version(dolfinx_version) >= Version("0.10.0"):
+        int_points = V.element.interpolation_points
+    else:
+        int_points = V.element.interpolation_points()
+
     u_bc = fem.Function(V)
-    u_bc.interpolate(fem.Expression(
-        u_exact, V.element.interpolation_points()))
+    u_bc.interpolate(fem.Expression(u_exact, int_points))
     bc = fem.dirichletbc(value=u_bc, dofs=dofs)
 
     # Define bilinear form and RHS
     u = TrialFunction(V)
     v = TestFunction(V)
-    a = dot(epsilon*grad(u), grad(v)) * dx + dot(beta, grad(u)) * v * dx
+    a = dot(epsilon * grad(u), grad(v)) * dx + dot(beta, grad(u)) * v * dx
     L = f * v * dx
 
     # Set-up linear problem
-    problem = fem.petsc.LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
-   
+    problem = fem.petsc.LinearProblem(
+        a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"}
+    )
+
     # Solve linear problem
     uh = problem.solve()
 
     # observables
     H1_error_form = fem.form(
-        epsilon * inner(grad(uh - u_exact), grad(uh - u_exact)) * dx + (uh - u_exact) ** 2 * dx
+        epsilon * inner(grad(uh - u_exact), grad(uh - u_exact)) * dx
+        + (uh - u_exact) ** 2 * dx
     )
     error_local = fem.assemble_scalar(H1_error_form)
     H1_error = np.sqrt(msh.comm.allreduce(error_local, op=MPI.SUM))
@@ -124,6 +157,7 @@ def BG_solve(
 
     return uh
 
+
 def PG_solve(
     m: int,
     polynomial_order: int,
@@ -132,7 +166,7 @@ def PG_solve(
     rho_value: float,
     epsilon: float,
     tol_exit: float,
-    GLL: bool
+    GLL: bool,
 ):
     """
     Solve the Eriksson–Johnson model problem with Proximal Galerkin
@@ -143,16 +177,23 @@ def PG_solve(
     msh = create_mesh(m)
 
     # Define FE subspaces
-    Q = ufl.FiniteElement("Lagrange", msh.ufl_cell(), polynomial_order)
-    mixed_element = ufl.MixedElement([Q, Q])
-    V = fem.FunctionSpace(msh, mixed_element)
+    if Version(dolfinx_version) >= Version("0.8.0"):
+        import basix.ufl
+
+        Q = basix.ufl.element("Lagrange", msh.basix_cell(), polynomial_order)
+        mixed_element = basix.ufl.mixed_element([Q, Q])
+        V = fem.functionspace(msh, mixed_element)
+    else:
+        Q = ufl.FiniteElement("Lagrange", msh.ufl_cell(), polynomial_order)
+        mixed_element = ufl.MixedElement([Q, Q])
+        V = fem.FunctionSpace(msh, mixed_element)
 
     # Define functions and parameters
     alpha = fem.Constant(msh, ScalarType(1))
     alpha.value = alpha_value
     rho = fem.Constant(msh, ScalarType(1))
     rho.value = rho_value
-    beta = fem.Constant(msh, ScalarType((1,0)))
+    beta = fem.Constant(msh, ScalarType((1, 0)))
     x = ufl.SpatialCoordinate(msh)
 
     f = fem.Constant(msh, 0.0)
@@ -163,23 +204,27 @@ def PG_solve(
     facets = mesh.exterior_facet_indices(msh.topology)
     V0, _ = V.sub(0).collapse()
     V1, _ = V.sub(1).collapse()
-    dofs_0 = fem.locate_dofs_topological(
-        (V.sub(0), V0), entity_dim=1, entities=facets)
-    dofs_1 = fem.locate_dofs_topological(
-        (V.sub(1), V1), entity_dim=1, entities=facets)
+    dofs_0 = fem.locate_dofs_topological((V.sub(0), V0), entity_dim=1, entities=facets)
+    dofs_1 = fem.locate_dofs_topological((V.sub(1), V1), entity_dim=1, entities=facets)
 
     u_bc = fem.Function(V0)
-    u_bc.interpolate(fem.Expression(
-        u_exact, V.sub(0).element.interpolation_points()))
+    if Version(dolfinx_version) >= Version("0.10.0"):
+        int_points = V.sub(0).element.interpolation_points
+    else:
+        int_points = V.sub(0).element.interpolation_points()
+    u_bc.interpolate(fem.Expression(u_exact, int_points))
     bc_0 = fem.dirichletbc(value=u_bc, dofs=dofs_0, V=V.sub(0))
 
     eps = 1e-3
-    u_exact_clip = max_value(eps, min_value(u_exact, 1-eps))
-    lnit_u_exact = ln(u_exact_clip/(1.0-u_exact_clip))
+    u_exact_clip = max_value(eps, min_value(u_exact, 1 - eps))
+    lnit_u_exact = ln(u_exact_clip / (1.0 - u_exact_clip))
 
     psi_bc = fem.Function(V1)
-    psi_bc.interpolate(fem.Expression(
-        lnit_u_exact, V.sub(1).element.interpolation_points()))
+    if Version(dolfinx_version) >= Version("0.10.0"):
+        int_points1 = V.sub(0).element.interpolation_points
+    else:
+        int_points1 = V.sub(0).element.interpolation_points()
+    psi_bc.interpolate(fem.Expression(lnit_u_exact, int_points1))
     bc_1 = fem.dirichletbc(value=psi_bc, dofs=dofs_1, V=V.sub(1))
 
     # Define solution variables
@@ -192,49 +237,50 @@ def PG_solve(
     # Define non-linear residual
     if GLL:
         print("\nUsing GLL quadrature")
-        dx_GLL = ufl.Measure("dx", metadata = {"quadrature_rule": "GLL","quadrature_degree": polynomial_order})
+        dx_GLL = ufl.Measure(
+            "dx",
+            metadata={"quadrature_rule": "GLL", "quadrature_degree": polynomial_order},
+        )
     else:
         dx_GLL = dx
     (v, w) = ufl.TestFunctions(V)
     F = (
-        alpha/rho * inner(grad(u), grad(v)) * dx
+        alpha / rho * inner(grad(u), grad(v)) * dx
         + psi * v * dx_GLL
         - alpha * (1 / rho - epsilon) * inner(grad(u_k), grad(v)) * dx
-        + alpha * dot(beta,grad(u_k)) * v * dx
+        + alpha * dot(beta, grad(u_k)) * v * dx
         - alpha * f * v * dx
         - psi_k * v * dx_GLL
         + u * w * dx_GLL
-        - (tanh(psi/2) + 1)/2 * w * dx_GLL
+        - (tanh(psi / 2) + 1) / 2 * w * dx_GLL
     )
     J = ufl.derivative(F, sol)
 
     # Setup non-linear problem
-    problem = NonlinearProblem(F, sol, bcs=[bc_0,bc_1], J=J)
+    problem = NonlinearProblem(F, sol, bcs=[bc_0, bc_1], J=J)
 
     # Setup Newton solver
     log.set_log_level(log.LogLevel.WARNING)
-    newton_solver = NewtonSolver(
-        comm=msh.comm, problem=problem)
+    newton_solver = NewtonSolver(comm=msh.comm, problem=problem)
 
     # Observables
-    L2primal_increment_form = fem.form( (u - u_k) ** 2 * dx )
-    L2latent_increment_form = fem.form(((tanh(psi/2) + 1)/2 - (tanh(psi_k/2) + 1)/2) ** 2 * dx)
+    L2primal_increment_form = fem.form((u - u_k) ** 2 * dx)
+    L2latent_increment_form = fem.form(
+        ((tanh(psi / 2) + 1) / 2 - (tanh(psi_k / 2) + 1) / 2) ** 2 * dx
+    )
     H1primal_error_form = fem.form(
-        epsilon * inner(grad(u - u_exact), grad(u - u_exact)) *
-        dx + (u - u_exact) ** 2 * dx
+        epsilon * inner(grad(u - u_exact), grad(u - u_exact)) * dx
+        + (u - u_exact) ** 2 * dx
     )
     L2primal_error_form = fem.form((u - u_exact) ** 2 * dx)
-    L2latent_error_form = fem.form(((tanh(psi/2) + 1)/2 - u_exact) ** 2 * dx)
+    L2latent_error_form = fem.form(((tanh(psi / 2) + 1) / 2 - u_exact) ** 2 * dx)
 
     # Initial guess from FEM solution
-    uh = BG_solve( m, polynomial_order, epsilon )
-    uh_clip = max_value(eps, min_value(uh, 1-eps))
-    lnit_uh = ln(uh_clip/(1.0-uh_clip))
-
-    sol.sub(0).interpolate(fem.Expression(
-        uh, V.sub(0).element.interpolation_points()))
-    sol.sub(1).interpolate(fem.Expression(
-        lnit_uh, V.sub(1).element.interpolation_points()))
+    uh = BG_solve(msh, polynomial_order, epsilon)
+    uh_clip = max_value(eps, min_value(uh, 1 - eps))
+    lnit_uh = ln(uh_clip / (1.0 - uh_clip))
+    sol.sub(0).interpolate(uh)
+    sol.sub(1).interpolate(fem.Expression(lnit_uh, int_points1))
 
     # Proximal residual outer loop
     n = 0
@@ -249,7 +295,6 @@ def PG_solve(
     primal_increments = []
     latent_increments = []
     for k in range(maximum_number_of_outer_loop_iterations):
-
         rank_print(f"OUTER LOOP {k + 1} alpha: {alpha.value}", msh.comm)
 
         # Solve problem
@@ -265,12 +310,17 @@ def PG_solve(
 
         tol_Newton = primal_increment
 
-        rank_print(f"‖u - uₕ‖_H¹: {H1primal_error}" +
-                   f"  ‖u - ũₕ‖_L² : {L2latent_error}", msh.comm)
+        rank_print(
+            f"‖u - uₕ‖_H¹: {H1primal_error}" + f"  ‖u - ũₕ‖_L² : {L2latent_error}",
+            msh.comm,
+        )
 
         if increment_k > 0.0:
-            rank_print(f"Increment size: {primal_increment}" +
-                       f"   Ratio: {primal_increment / increment_k}", msh.comm)
+            rank_print(
+                f"Increment size: {primal_increment}"
+                + f"   Ratio: {primal_increment / increment_k}",
+                msh.comm,
+            )
         else:
             rank_print(f"Increment size: {primal_increment}", msh.comm)
         rank_print("", msh.comm)
@@ -315,31 +365,29 @@ def PG_solve(
     df.to_csv(output_dir / filename, index=False)
     rank_print(df, msh.comm)
 
-
     # Export primal solution variable
-    V_out = fem.FunctionSpace(msh, ("Lagrange", polynomial_order))
-    q = fem.Function(V_out)
-    q.interpolate(sol.sub(0).collapse())
-    with io.VTXWriter(msh.comm, output_dir / "u.bp", [q]) as vtx:
+    with io.VTXWriter(msh.comm, output_dir / "u.bp", [sol.sub(0).collapse()]) as vtx:
         vtx.write(0.0)
 
     # Export interpolant of exact solution u
-    expr = fem.Expression(u_exact, V_out.element.interpolation_points())
+    expr = fem.Expression(u_exact, int_points)
+    V_primal, _ = V.sub(0).collapse()
+    q = fem.Function(V_primal)
     q.interpolate(expr)
     with io.VTXWriter(msh.comm, output_dir / "u_exact.bp", [q]) as vtx:
         vtx.write(0.0)
 
     # Export latent solution variable
-    q.interpolate(sol.sub(1).collapse())
-    with io.VTXWriter(msh.comm, output_dir / "psi.bp", [q]) as vtx:
+    with io.VTXWriter(msh.comm, output_dir / "psi.bp", [sol.sub(1).collapse()]) as vtx:
         vtx.write(0.0)
 
     # Export feasible discrete solution
-    exp_psi = (tanh(sol.sub(1)/2) + 1)/2
-    expr = fem.Expression(exp_psi, V_out.element.interpolation_points())
+    exp_psi = (tanh(sol.sub(1) / 2) + 1) / 2
+    expr = fem.Expression(exp_psi, int_points)
     q.interpolate(expr)
     with io.VTXWriter(msh.comm, output_dir / "tilde_u.bp", [q]) as vtx:
         vtx.write(0.0)
+
 
 # -------------------------------------------------------
 if __name__ == "__main__":
@@ -420,5 +468,5 @@ if __name__ == "__main__":
         args.rho,
         args.epsilon,
         args.tol_exit,
-        args.GLL
+        args.GLL,
     )
